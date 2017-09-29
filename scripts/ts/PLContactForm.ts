bd54abee89c63ca9b47d1b528187f339a8f02caf
sexar <iconfidence@gmail.com>
2017-09-29 00:17:57 -0500

    1. Change static methods by instance methods 2. Add Events to each state of XMLHttpRequest 3. Rename some methods.

--- scripts/ts/PLContactForm.ts
+++ scripts/ts/PLContactForm.ts
@@ -1,67 +1,182 @@
 /**
  * Created by cesarmejia on 20/08/2017.
- * Considerar que el formulario también sea sincrono.
+ * TODO: 1. Add functionality to send form synchronous.
+ *       2. Consider sending files
  */
 module pl {
 
     export class ContactForm {
 
+        // region Fields
+        /**
+         * Error event.
+         * @type {Event}
+         */
+        private _error: Event;
+
         /**
          * Points to form element.
          * @type {HTMLFormElement}
-         * @private
          */
         private _form: HTMLFormElement;
 
         /**
-         * Store custom handlers for each event in ajax request.
-         * @type {object}
-         * @private
-         */
-        private _handlers: Object = {};
-
-        /**
          * Points to form inputs.
          * @type {NodeListOf<Element>}
-         * @private
          */
         private _inputs: NodeListOf<Element>;
 
         /**
          * Determine if window could close or not.
          * @type {boolean}
-         * @private
          */
         private _letCloseWindow: boolean = true;
 
         /**
          * Object that will be used to make requests.
          * @type {XMLHttpRequest}
-         * @private
          */
         private _req: XMLHttpRequest = new XMLHttpRequest;
 
         /**
+         * Sending event.
+         * @type {Event}
+         */
+        private _sending: Event;
+
+        /**
          * Contains info for contact form.
          * @type {object}
-         * @private
          */
         private _settings: Object = {};
 
         /**
-         * Get form inputs.
-         * @param form
-         * @returns {NodeListOf<Element>}
+         * Success event.
+         * @type {Event}
          */
-        static getInputs(form: HTMLFormElement) {
-            let validInputs = [
-                "input[type=text]",
-                "input[type=checkbox]",
-                "input[type=radio]",
-                "textarea"
-            ];
+        private _success: Event;
+        // endregion
 
-            return form.querySelectorAll( validInputs.join(",") );
+        /**
+         * Create a contact form instance.
+         * @param {HTMLElement} form
+         * @param {object} settings
+         */
+        constructor(form: HTMLFormElement, settings: Object = {}) {
+            if (!(form instanceof HTMLElement))
+                throw 'Template is not an HTMLElement';
+
+            let defaults = {
+                method: 'POST',
+                url: 'send-mail.php',
+                async: true
+            };
+
+            this._form = form;
+            this._inputs = this.getInputs();
+            this._settings = Util.extendsDefaults(defaults, settings);
+
+            this.initializeEvents();
+        }
+
+        // region Private Methods
+        /**
+         * Make an ajax request with contact form data.
+         * @param {object} data
+         */
+        private ajaxRequest(data) {
+            let settings = this._settings;
+            let dataString = `data=${JSON.stringify(data)}`;
+
+            this.onSending();
+
+            this._req.open(settings['method'], settings['url'], settings['async']);
+            this._req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            this._req.send(dataString);
+
+        }
+
+        /**
+         * Shows message while contact form is working
+         * and avoid user closes the window.
+         */
+        private beforeUnload() {
+            if (!this._letCloseWindow) {
+                return 'Sending message';
+            }
+        }
+
+        /**
+         * Handle input change event.
+         * @param {Event} ev
+         */
+        private changed(ev) {
+            let code = ev.which || ev.keyCode || 0;
+
+            // Do nothing if key is invalid.
+            if (this.isInvalidKey(code)) return;
+
+            // Retrieve input and some attrs.
+            let input: HTMLInputElement = ev.target;
+
+            // Show or hide error.
+            this.toggleInputError(input);
+        }
+
+        /**
+         * Handles state changes of request.
+         * @param {Event} ev
+         */
+        private handleReadyStateChange(ev) {
+            let DONE = 4; // readyState 4 means the request is done.
+            let OK   = 200; // status 200 is a successful return.
+
+            if (this._req.readyState === DONE) {
+                if (this._req.status === OK) {
+                    this.onSuccess(
+                        this._req.responseText,
+                        this._req.status,
+                        this._req.statusText
+                    );
+                } else {
+                    this.onError(
+                        this._req.status,
+                        this._req.statusText
+                    );
+                }
+            }
+        }
+
+        /**
+         * Attach handlers to contact form elements.
+         */
+        private initializeEvents() {
+            this.beforeUnload = this.beforeUnload.bind(this);
+            this.changed      = this.changed.bind(this);
+            this.submit       = this.submit.bind(this);
+            this.handleReadyStateChange = this.handleReadyStateChange.bind(this);
+
+
+            // Attach changed handler to each input in form.
+            [].forEach.call(this._inputs, (input) => {
+                if (input.type === 'text' || input.tagName.toLowerCase() === 'textarea')
+                    input.addEventListener('keyup', this.changed, false);
+
+                input.addEventListener('change', this.changed, false);
+            });
+
+
+            // Attach on submit handler to form.
+            this._form.addEventListener('submit', this.submit, false);
+
+
+            // Attach onbeforeunload handler.
+            window.onbeforeunload = this.beforeUnload;
+
+
+            // Attach handler to state change of request.
+            this._req.onreadystatechange = this.handleReadyStateChange;
+
         }
 
         /**
@@ -69,7 +184,7 @@ module pl {
          * @param {HTMLInputElement} input
          * @returns {boolean} validity
          */
-        static isInputValid(input: HTMLInputElement) {
+        private isInputValid(input: HTMLInputElement) {
             let validate = input.dataset['validate'],
                 name  = input.name,
                 value = input.value;
@@ -92,19 +207,19 @@ module pl {
          * Return if code is an invalid key.
          * @param {number} code
          */
-        static isInvalidKey(code: number) {
+        private isInvalidKey(code: number) {
             let i, invalidKeys = [
-                    Key.ALT,
-                    Key.CAPS_LOCK,
-                    Key.CTRL,
-                    Key.DOWN_ARROW,
-                    Key.LEFT_ARROW,
-                    Key.RIGHT_ARROW,
-                    Key.SELECT,
-                    Key.SHIFT,
-                    Key.UP_ARROW,
-                    Key.TAB
-                ];
+                Key.ALT,
+                Key.CAPS_LOCK,
+                Key.CTRL,
+                Key.DOWN_ARROW,
+                Key.LEFT_ARROW,
+                Key.RIGHT_ARROW,
+                Key.SELECT,
+                Key.SHIFT,
+                Key.UP_ARROW,
+                Key.TAB
+            ];
 
             for (i = 0; i < invalidKeys.length; i++) {
                 if (invalidKeys[i] === code)
@@ -118,14 +233,14 @@ module pl {
          * Add or remove error from input
          * @param {HTMLElement} input
          */
-        static toggleInputError(input) {
+        private toggleInputError(input) {
             let type : String = input['type'];
 
             // If input has an error get it.
             let clueElem: HTMLElement = input['clue-elem'];
             let clueText: string = "";
 
-            if (ContactForm.isInputValid(input)) {
+            if (this.isInputValid(input)) {
 
                 if (clueElem) {
                     // Disappears and remove error element from DOM.
@@ -162,62 +277,14 @@ module pl {
 
             }
         }
+        // endregion
 
+        // region Methods
         /**
-         * Create a contact form instance.
-         * @param {HTMLElement} form
-         * @param {object} settings
-         */
-        constructor(form: HTMLFormElement, settings: Object = {}) {
-            if (!(form instanceof HTMLElement))
-                throw 'Template is not an HTMLElement';
-
-            let defaults = {
-                method: 'POST',
-                url: 'send-mai.php',
-                async: true
-            };
-
-            this._form = form;
-            this._inputs = ContactForm.getInputs(this._form);
-            this._settings = Util.extendsDefaults(defaults, settings);
-
-            this.initializeEvents();
-        }
-
-        /**
-         * Make an ajax request with contact form data.
-         * @param {object} data
-         */
-        private ajaxRequest(data) {
-            let settings = this._settings;
-            let dataString = `data=${JSON.stringify(data)}`;
-
-            /* this._req.onreadystatechange = function () {
-
-            }; */
-
-            this._req.open(settings['method'], settings['url'], settings['async']);
-            this._req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            this._req.send(dataString);
-
-        }
-
-        /**
-         * Shows message while contact form is working
-         * and avoid user closes the window.
-         */
-        private beforeUnload() {
-            if (!this._letCloseWindow) {
-                return 'Sending message';
-            }
-        }
-
-        /**
-         * Gets an object with form values.
+         * Gets all values of inputs in JSON format.
          * @returns {object}
          */
-        private getFormData() {
+        public getFormValues() {
             let data = { };
 
             [].forEach.call(this._inputs, (input) => {
@@ -228,125 +295,36 @@ module pl {
         }
 
         /**
-         * Handle progress state.
+         * Get form inputs.
+         * @returns {NodeListOf<Element>}
          */
-        private handleProgress(ev) {
-            console.log('progress');
-            console.log(ev);
+        public getInputs() {
+            let validInputs = [
+                "input[type=text]",
+                "input[type=checkbox]",
+                "input[type=radio]",
+                "select",
+                "textarea"
+            ];
+
+            return this._form.querySelectorAll( validInputs.join(",") );
         }
 
         /**
-         * Handle failed state.
+         * Validates all inputs in the form.
+         * @returns {boolean}
          */
-        private handleFailed(ev) {
-            console.log('failed');
-            console.log(ev);
-        }
+        public isFormValid() {
+            let valid = true;
 
-        /**
-         * Handle success state.
-         */
-        private handleSuccess(ev) {
-            console.log('success');
-            console.log(ev);
-
-            console.log(this._req.response);
-            console.log(this._req.responseText);
-        }
-
-        /**
-         * Attach handlers to contact form elements.
-         */
-        private initializeEvents() {
-            this.handleFailed   = this.handleFailed.bind(this);
-            this.handleProgress = this.handleProgress.bind(this);
-            this.handleSuccess  = this.handleSuccess.bind(this);
-
-            this.beforeUnload  = this.beforeUnload.bind(this);
-            this.onInputChange = this.onInputChange.bind(this);
-            this.onSubmit      = this.onSubmit.bind(this);
-
-            // Attach onbeforeunload handler.
-            window.onbeforeunload = this.beforeUnload;
-
-
-            // Attach onInputChange handler to each input in form.
-            [].forEach.call(this._inputs, (input) => {
-                if (input.type === 'text' || input.tagName.toLowerCase() === 'textarea')
-                    input.addEventListener('keyup', this.onInputChange, false);
-
-                input.addEventListener('change', this.onInputChange, false);
-            });
-
-
-            // Attach on Submit handler to form.
-            this._form.addEventListener('submit', this.onSubmit, false);
-
-
-            // Attach handlers to request events.
-            this._req.addEventListener('error', this.handleFailed, false);
-            this._req.addEventListener('loadstart', this.handleProgress, false);
-            this._req.addEventListener('load', this.handleSuccess, false);
-
-        }
-
-        /**
-         * Handle input change event.
-         * @param {Event} ev
-         */
-        private onInputChange(ev) {
-            let code = ev.which || ev.keyCode || 0;
-
-            // Do nothing if key is invalid.
-            if (ContactForm.isInvalidKey(code)) return;
-
-            // Retrieve input and some attrs.
-            let input: HTMLInputElement = ev.target;
-
-            // Show or hide error.
-            ContactForm.toggleInputError(input);
-        }
-
-        /**
-         * Handle submit event.
-         * @param {Event} ev
-         */
-        private onSubmit(ev) {
-            ev.preventDefault();
-
-            let form  = ev.target,
-                valid = true;
-
-            // Validate inputs before submit.
-            [].forEach.call(this._inputs, (input) => {
-                if (!ContactForm.isInputValid(input)) {
-                    ContactForm.toggleInputError(input);
+            [].forEach.call(this._inputs, input => {
+                if (!this.isInputValid(input)) {
+                    this.toggleInputError(input);
                     valid = false;
                 }
             });
 
-            if (!valid) {
-                throw "There are invalid inputs.";
-
-            } else {
-
-                let data = {
-                    host: location.hostname,
-                    data: this.getFormData()
-                };
-
-                this.ajaxRequest(data);
-            }
-
-        }
-
-        /**
-         * Add custom handler.
-         * @param {string} name before|error|success
-         * @param {function} handler
-         */
-        public addHandler(name, handler) {
-            this._handlers[name] = handler;
+            return valid;
         }
 
         /**
@@ -355,6 +333,110 @@ module pl {
         public reset() {
             this._form.reset();
         }
+
+        /**
+         * Handle submit event.
+         * @param {Event} ev
+         */
+        public submit(ev) {
+            // Maybe submit is called manually and there is no ev.
+            ev && ev.preventDefault();
+
+            if (!this.isFormValid()) {
+                throw "There are invalid inputs.";
+
+            } else {
+
+                let data = {
+                    host: location.hostname,
+                    data: this.getFormValues()
+                };
+
+                this.ajaxRequest(data);
+            }
+
+        }
+        // endregion
+
+        // region Events
+        /**
+         * Fires when contact form request has an error.
+         * @param {number} status
+         * @param {string} statusText
+         */
+        private onError(status, statusText) {
+            if (this._error) {
+                this._error.fire(status, statusText);
+            }
+
+            this._letCloseWindow = true;
+        }
+
+        /**
+         * Fires when contact form is sending.
+         */
+        private onSending() {
+            if (this._sending) {
+                this._sending.fire();
+            }
+
+            this._letCloseWindow = false;
+        }
+
+        /**
+         * Fires when contact form request was success.
+         * @param {string} response
+         * @param {number} status
+         * @param {string} statusText
+         */
+        private onSuccess(response, status, statusText) {
+            if (this._success) {
+                this._success.fire(response, status, statusText);
+            }
+
+            this._letCloseWindow = true;
+
+            this.reset();
+        }
+        // endregion
+
+        // region Properties
+        /**
+         * Get error event.
+         * @returns {Event}
+         */
+        public get error() {
+            if (!this._error) {
+                this._error = new Event();
+            }
+
+            return this._error;
+        }
+
+        /**
+         * Get sending event
+         * @returns {Event}
+         */
+        public get sending() {
+            if (!this._sending) {
+                this._sending = new Event();
+            }
+
+            return this._sending;
+        }
+
+        /**
+         * Get success event.
+         * @returns {Event}
+         */
+        public get success() {
+            if (!this._success) {
+                this._success = new Event();
+            }
+
+            return this._success;
+        }
+        // endregion
 
     }
 
